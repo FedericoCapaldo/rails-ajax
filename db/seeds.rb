f20ff173a22068127b30217f4d155448bcc2d186
Federico Capaldo <federico.capaldo1@gmail.com>
2021-11-18 18:12:32 +0000

    Seed 2 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants...'
+Restaurant.create!({
+                     name: "Le Dindon en Laisse",
+                     address: "18 Rue Beautreillis, 75004 Paris, France"
+                   })
+Restaurant.create!({
+                     name: "Neuf et Voisins",
+                     address: "Van Arteveldestraat 1, 1000 Brussels, Belgium"
+                   })
+puts 'Finished!'
