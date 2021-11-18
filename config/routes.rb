df2b84d023ff9bc0b992946959e430e37087e9a9
Federico Capaldo <federico.capaldo1@gmail.com>
2021-11-18 19:31:50 +0000

    added the ability to delete a restaurant while using ajax

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :restaurants, only: [ :index, :show ] do
+  resources :restaurants, only: [ :index, :show, :destroy ] do
     resources :reviews, only: :create
   end
 
