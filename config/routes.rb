1e35bed1e13e5f158a4183b13ba7be4697b51332
Federico Capaldo <federico.capaldo1@gmail.com>
2021-11-18 20:51:08 +0000

    OPTIONAL: MANUAL XHR/FETCH EXAMPLES to understand xhr, fetch, async/await in depth

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
   resources :restaurants, only: [ :index, :show, :destroy ] do
     resources :reviews, only: :create
   end
 
+  # extra examples to understand AJAX manually, for clarity
+  get 'xhr-example', to: 'pages#xhr_example'
+  get 'fetch-example', to: 'pages#fetch_example'
+  get 'fetch-example-modern', to: 'pages#fetch_example_modern'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
