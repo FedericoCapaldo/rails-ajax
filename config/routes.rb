3622ab43cbe9cdf8b7407f549f77985958be0bad
Federico Capaldo <federico.capaldo1@gmail.com>
2021-11-18 18:17:36 +0000

    Add Restaurant index & show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  resources :restaurants, only: [ :index, :show ]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
